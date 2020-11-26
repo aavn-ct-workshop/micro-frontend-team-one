387533589a9edf2163dc57b999d3046028232e9a
Phong Pham Nguyen Quang <phong.pham@axonactive.com>
2020-11-26 13:10:20 +0700

    Sandbox teamone

--- sandbox.js
+++ sandbox.js
@@ -20,10 +20,41 @@ for (const key in fakeProducts) {
                     </div>
                     <div class="pricing-table-footer">
                         <button class="sell-button pure-button" data-product="${key}">Sell</button>
-                        <button class="buy-button pure-button pure-button-primary" data-product="${key}">Buy</button>
+                        <button class="buy-button pure-button pure-button-primary" data-product="${key}" data-price="${element.price}">Buy</button>
                     </div>
                 </div>
             </div>`;
     }
 }
+
+// Simulate case team 3 update receipt total
+document.getElementsByClassName('dummy-total-1000000')[0]?.addEventListener("click", () => {
+    this.dispatchEvent(
+        new CustomEvent("team-three-update-total", {
+            bubbles: true,
+            detail: { 'amount': 1000000 }
+        })
+    );
+});
+
+document.getElementsByClassName('dummy-total-9999999')[0]?.addEventListener("click", () => {
+    this.dispatchEvent(
+        new CustomEvent("team-three-update-total", {
+            bubbles: true,
+            detail: { 'amount': 9999999 }
+        })
+    );
+});
+
+// Simulate case team 1 receive current amount
+window.addEventListener("team-one-current-amount", (e) => {
+    let buyButtons = document.getElementsByClassName('buy-button');
+    for (var i = 0; i < buyButtons.length; i++) {
+        if (buyButtons[i]?.getAttribute("data-price") > e.detail['amount']) {
+            buyButtons[i]?.classList.add('button-disable');
+        } else {
+            buyButtons[i]?.classList.remove('button-disable');
+        }
+    }
+});
 
